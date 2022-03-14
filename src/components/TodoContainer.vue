f79cca004932ba1e2ffaa9389b1376e71e77c80d
dmsrb <ddmsbb@naver.com>
2022-03-14 19:26:02 +0900

    work

--- src/components/TodoContainer.vue
+++ src/components/TodoContainer.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
+    <b-button @click="addToDo">
+      추가
+    </b-button>
+    <b-form-input
+      v-model="text"
+      placeholder="Enter Todo"
+    />
+    <div class="mt-2">
+      Value: {{ text }}
+    </div>
     <h1>TodoContainer</h1>
-    <TodoItem />
+    <TodoItem
+      v-for="msg in todoList"
+      :key="msg"
+      :msg="msg"
+    />
+    </b-button@click>
   </div>
 </template>
 
@@ -15,5 +30,12 @@ import TodoItem from '@/components/TodoItem.vue';
   },
 })
 export default class TodoContainer extends Vue {
+  private todoList:Array<string> = ['test1', 'test2', 'test3']
+
+  private text = ''
+
+  addToDo():void {
+    this.todoList.push();
+  }
 }
 </script>
